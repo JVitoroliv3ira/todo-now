6efb7ca2cba094a99cc041cd210822c066050e43
João Oliveira <jvitoroliveira0711@gmail.com>
2024-06-30 12:15:11 -0300

    app: Adiciona conexão co o banco de dados.

--- todo-now-api/src/TodoNow/TodoNow.Api/Program.cs
+++ todo-now-api/src/TodoNow/TodoNow.Api/Program.cs
@@ -1,10 +1,31 @@
+using Microsoft.EntityFrameworkCore;
+using TodoNow.Infra.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddControllers();
 
+var connectionString = builder.Configuration.GetConnectionString("SqlServerConnection");
+
+builder.Services.AddDbContext<ApplicationDbContext>(o => o.UseSqlServer(connectionString));
+
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    var context = services.GetRequiredService<ApplicationDbContext>();
+    var migracaoConnectionString = builder.Configuration.GetConnectionString("MigracaoSqlServerConnection");
+
+    if (!string.IsNullOrEmpty(migracaoConnectionString))
+    {
+        context.Database.SetConnectionString(migracaoConnectionString);
+    }
+
+    context.Database.Migrate();
+}
 
 if (app.Environment.IsDevelopment())
 {
