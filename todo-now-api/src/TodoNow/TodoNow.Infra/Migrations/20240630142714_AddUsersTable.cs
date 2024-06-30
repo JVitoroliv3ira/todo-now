505093dd3fee26ee76cae611c829ab93c5fee244
João Oliveira <jvitoroliveira0711@gmail.com>
2024-06-30 12:14:52 -0300

    app: Adiciona migração responsável por criar a tabela de usuarios.

---
+++ todo-now-api/src/TodoNow/TodoNow.Infra/Migrations/20240630142714_AddUsersTable.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TodoNow.Infra.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUsersTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "tb_users",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    name = table.Column<string>(type: "nvarchar(80)", nullable: false),
+                    email = table.Column<string>(type: "nvarchar(100)", nullable: false),
+                    password = table.Column<string>(type: "nvarchar(100)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_users", x => x.id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "tb_users");
+        }
+    }
+}
